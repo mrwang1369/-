aae1fa82dee129f2410379cde1bffcaca411544e
MrWang <2287749325@qq.com>
2026-02-08 18:30:45 +0800

    初步创建骨架

---
+++ src/main/java/com/pethealth/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.pethealth;  // 包名应该是 com.pethealth
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.pethealth.mapper")
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
